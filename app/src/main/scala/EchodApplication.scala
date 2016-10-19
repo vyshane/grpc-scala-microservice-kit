d2f867d85561e9fb7a97f41a2d9e617b52bc0e80
Vy-Shane Xie <shane@node.mu>
2016-10-19 20:25:24 +0800

    Log message on gRPC server startup.

--- app/src/main/scala/EchodApplication.scala
+++ app/src/main/scala/EchodApplication.scala
@@ -1,10 +1,13 @@
 package mu.node.echod
 
+import org.slf4j.LoggerFactory
+
 /*
  * Main entry point for the application
  */
 object EchodApplication extends EchodModule {
   def main(args: Array[String]): Unit = {
+    LoggerFactory.getLogger(this.getClass).info("Starting gRPC server")
     echoServer.start().awaitTermination()
   }
 }
