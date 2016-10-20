d9b0a49e217469ed68a580d699ec85c0036a3a01
Vy-Shane Xie <shane@node.mu>
2016-10-20 21:21:22 +0800

    This is a def.

--- app/src/main/scala/grpc/UserContextServerInterceptor.scala
+++ app/src/main/scala/grpc/UserContextServerInterceptor.scala
@@ -42,5 +42,5 @@ class UserContextServerInterceptor(jwtVerificationKey: PublicKey) extends Server
 }
 
 object UserContextServerInterceptor {
-  val userContextKey: Context.Key[UserContext] = Context.key("user_context")
+  def userContextKey: Context.Key[UserContext] = Context.key("user_context")
 }
